3ad3d515be9e27fe9afb35c5259c9894fa52518c
hasegawa-div <monkeygorillahippo@gmail.com>
2026-06-22 23:07:03 +0900

    Remove practice files

--- crane.py
+++
@@ -1,6 +0,0 @@
-total_legs = float(input('足の数を入力してください'))
-num_turtles = float(input('亀の数を入力してください'))
-
-num_cranes = (total_legs - 4*num_turtles) // 2
-print(num_cranes)
-
--- vote.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return '投票サイトへようこそ' 
-
-app.debug = True
-app.run()
